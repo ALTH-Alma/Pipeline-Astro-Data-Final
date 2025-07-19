94d4deba0c7f87a7aed06d8f3c19d9f3904c6d61
ALTH <73628637+ALTH-Alma@users.noreply.github.com>
2025-07-19 18:37:30 -0400

    Update Alma_restart_par_new.py

--- FARGO/Alma_restart_par_new.py
+++ FARGO/Alma_restart_par_new.py
@@ -1,3 +1,17 @@
+### Este script modifica los archivos de configuración de parámetros para reiniciar simulaciones.
+### Copia archivos .par existentes y ajusta los valores de DT, Ninterm y Ntot
+### para correr simulaciones más cortas a partir de un punto previo
+### y obtener 20 salidas nuevas cada cierto intervalo de tiempo (según el valor de DT).
+
+## DT: Es el tamaño del paso temporal (timestep) en las unidades de tiempo internas del código.
+## 2π unidades de tiempo = 1 año orbital a 1 AU (unidad astronomica).
+
+## Ntot: Índica cuánto tiempo dura la simulación en total (en pasos de tiempo).
+## Nintem: Índica cada cuánto tiempo se guardan datos (en pasos de tiempo). 
+
+### Cada 10 pasos de tiempo de ~0,0017799 unidades de tiempo (0.0103 días) se guarda una salida,
+### hasta alcanzar los 200 pasos de tiempo (19,6 días). (10 pasos = 1.03 días)
+
 import os, fileinput, sys
 
 N = 500 # Número de simulaciones
