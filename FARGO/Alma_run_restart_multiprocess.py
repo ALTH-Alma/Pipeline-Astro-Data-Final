81079e83e88fa4b6f36fcc733ea123d059d8f213
ALTH <73628637+ALTH-Alma@users.noreply.github.com>
2025-07-19 20:10:28 -0400

    Update Alma_run_restart_multiprocess.py

--- FARGO/Alma_run_restart_multiprocess.py
+++ FARGO/Alma_run_restart_multiprocess.py
@@ -1,1 +1,11 @@
+# Corre el restart de las simulaciones para la configuración miltiprocess
 
+import subprocess
+
+N = 100 # Número de simulaciones
+offset = 0 # Offset de simulación
+restart_file = 1 # Archivo salida de la simulacion desde la que se empieza el restart (desde la primera)
+
+for sim in range(N):
+    new_par_file = 'setups/Alma_Vidal_new/Alma_restart_new_{:d}.par'.format(sim+offset)
+    subprocess.run(["time", "./fargo3d", "-S", str(restart_file), new_par_file])
