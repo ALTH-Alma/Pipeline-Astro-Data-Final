63ea5b25ce6a83487336b01f2986a5ddfeaa5537
ALTH <73628637+ALTH-Alma@users.noreply.github.com>
2025-07-19 20:04:03 -0400

    Update Alma_run_restart.py

--- FARGO/Alma_run_restart.py
+++ FARGO/Alma_run_restart.py
@@ -1,10 +1,11 @@
+# Corre el restart de las simulaciones para la configuración GPU
+
 import subprocess
 
-device = 0 
-N = 13
-offset = 287
-restart_file = 1
+N = 100 # Número de simulaciones
+offset = 0 # Offset de simulación
+restart_file = 1 # Archivo salida de la simulacion desde la que se empieza el restart (desde la primera)
+
 for sim in range(N):
-    new_par_file = 'setups/Alma_Vidal/Alma_restart_{:d}.par'.format(sim+offset)
-    #Run simulation
-    subprocess.run(["time", "./fargo3d", "-D", str(device), "-S", str(restart_file),new_par_file])
+    new_par_file = 'setups/Alma_Vidal_new/Alma_restart_new_{:d}.par'.format(sim+offset)
+    subprocess.run(["time", "./fargo3d", "-S", str(restart_file), new_par_file])
